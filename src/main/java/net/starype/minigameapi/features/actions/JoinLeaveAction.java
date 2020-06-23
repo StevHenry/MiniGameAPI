f58edde2693e48e7a268d3e367a89e3631c57974
Steven HENRY <stevenhenry83270@gmail.com>
2020-06-23 11:46:27 +0200

    Added missing import (for javadoc)

--- src/main/java/net/starype/minigameapi/features/actions/JoinLeaveAction.java
+++ src/main/java/net/starype/minigameapi/features/actions/JoinLeaveAction.java
@@ -1,12 +1,13 @@
 package net.starype.minigameapi.features.actions;
 
+import net.starype.minigameapi.multilinkable.StateChangeAction;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import net.starype.minigameapi.features.standard.GameDivision;
 
 /**
- * <p>Interace that is used in the {@link GameDivision#addStep(StateChangeAction)} function in class 
+ * <p>Interace that is used in the {@link GameDivision#addStep(StateChangeAction)} function in class
  * {@link GameDivision}</p>
  * 
  * @author Askigh
